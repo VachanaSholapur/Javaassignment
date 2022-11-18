1a7f7b446343f8e72bb409c614e9d078e453b59e
VachanaSholapur <89834476+VachanaSholapur@users.noreply.github.com>
2022-11-18 09:54:23 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,16 @@
+public class Time {
+    public static void main(String[] args) {
+
+
+
+            int hour = 23;
+            int min = 59;
+            int sec = 59;
+
+            for (int i = 0; i <= hour; i++) ;
+            {
+                System.out.println(hour+":"+ hour+":"+ hour);
+            }
+
+    }
+}
